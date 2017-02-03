c3f53f273a3206780e9b06685ca284612e6d708a
Quyen <boy20195@gmail.com>
2017-02-03 13:35:38 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs'); 
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my Page'
+    });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+      pageTitle: 'About Page'
+  }); 
+});
+
+app.get('/bad', (req, res) => {
+   res.send({
+      errorMessage: 'Unable to handle request.' 
+   }); 
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is up on port 3000.');
+});
